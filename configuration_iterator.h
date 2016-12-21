3c116ed73dce58ae08fa3046837a57fcb957af2d
Nick Mayhall <nmayhall@vt.edu>
2016-12-21 07:12:23 -0500

    added single header file to collect all other header files

---
+++ configuration_iterator.h
@@ -0,0 +1,11 @@
+
+#ifndef CONFIGURATION_ITERATOR_H
+#define CONFIGURATION_ITERATOR_H
+
+#include "./helpers.h"
+#include "./CombinatorialIndex.h"
+#include "./OrbitalSpaces.h"
+#include "./DeterminantSpace.h"
+#include "./Determinant.h"
+
+#endif
